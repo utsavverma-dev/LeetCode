9727f18c59b4f802f8c177e72138a1c1aa220c02
Utsav Verma <158056139+utsavverma-dev@users.noreply.github.com>
2026-07-22 14:45:20 +0530

    Time: 29 ms (17.29%), Space: 54.1 MB (20.38%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,15 +5,16 @@ class Solution {
 
         // Approach 2
 
-        // int[][] memo=new int [text1.length()][text2.length()];
-        // for(int i=0;i<text1.length();i++)
-        // {
-        //     for(int j=0;j<text2.lenght();j++)
-        //     {
-        //         memo[i][j]=-1;
-        //     }
-        // }
-        // return f(text1,text2,text1.length()-1,text2.length()-1,int[][] memo);
+        int[][] memo=new int [text1.length()][text2.length()];
+        for(int i=0;i<text1.length();i++)
+        {
+            for(int j=0;j<text2.length();j++)
+            {
+                memo[i][j]=-1;
+            }
+        }
+        return f(text1,text2,text1.length()-1,text2.length()-1, memo);
+    }
 
         // Approach 3 Tabulation 
         // int[][]dp=new int[text1.length()+1][text2.length()+1];
@@ -33,37 +34,37 @@ class Solution {
         // return dp[text1.length()][text2.length()];
 
         // Approach 4 SpaceOptimisation DP
-        int m=text1.length();
-        int n=text2.length();
-        int[]pdp=new int[text2.length()+1];
+    //     int m=text1.length();
+    //     int n=text2.length();
+    //     int[]pdp=new int[text2.length()+1];
 
-        for(int i=1;i<text1.length()+1;i++){
-             int[]cdp=new int[text2.length()+1];
-            for(int j=1;j<text2.length()+1;j++)
-            {
-                if(text1.charAt(i-1)==text2.charAt(j-1))
-                cdp[j]=1+pdp[j-1];
+    //     for(int i=1;i<text1.length()+1;i++){
+    //          int[]cdp=new int[text2.length()+1];
+    //         for(int j=1;j<text2.length()+1;j++)
+    //         {
+    //             if(text1.charAt(i-1)==text2.charAt(j-1))
+    //             cdp[j]=1+pdp[j-1];
 
-                else
-                cdp[j]=Math.max(pdp[j],cdp[j-1]);
-            }
-            pdp=cdp;
-        }
+    //             else
+    //             cdp[j]=Math.max(pdp[j],cdp[j-1]);
+    //         }
+    //         pdp=cdp;
+    //     }
 
-        return pdp[text2.length()];
-    }
+    //     return pdp[text2.length()];
+    // }
 
-    int f(String t1,String t2,int i, int j)
-    {
-        if(i<0 || j<0)
-        return 0;
+    // int f(String t1,String t2,int i, int j)
+    // {
+    //     if(i<0 || j<0)
+    //     return 0;
 
-        if(t1.charAt(i)== t2.charAt(j))
-        return 1+f(t1,t2,i-1,j-1);
+    //     if(t1.charAt(i)== t2.charAt(j))
+    //     return 1+f(t1,t2,i-1,j-1);
 
-        else 
-        return Math.max(f(t1,t2,i-1,j),f(t1,t2,i,j-1));
-    }
+    //     else 
+    //     return Math.max(f(t1,t2,i-1,j),f(t1,t2,i,j-1));
+    // }
     int f(String t1,String t2,int i, int j,int[][] memo)
     {
         if(i<0 || j<0)
