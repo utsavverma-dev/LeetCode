606948d8ab9e61ac5709bf0fd2db9b504d5491d9
Utsav Verma <158056139+utsavverma-dev@users.noreply.github.com>
2026-08-03 22:20:58 +0530

    Time: 2 ms (93.98%), Space: 71.9 MB (53.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,47 +1,47 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int pro=1;
-       int zc=0;
-       for(int i=0;i<nums.length;i++)
-       {
-        if(nums[i]==0)
-        {
-            zc++;
-        }
-        else
-        pro*=nums[i];
-       }
+        int zc=0;
+        int pro=1;
 
-       if(zc==0)
-       {
-        for(int i=0;i<nums.length;i++)
+        for(int n:nums)
         {
-            nums[i]=pro/nums[i];
-        }
-       }
-       else if(zc==1)
-       {
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==0)
+            if(n==0)
             {
-              nums[i]=pro;
+                zc++;
             }
             else
+            pro=pro*n;
+        }
+
+
+        if(zc==0)
+        {
+            for(int i=0;i<nums.length;i++)
+            {
+                nums[i]=pro/nums[i];
+            }
+
+        }
+        else if(zc==1)
+        {
+            for(int i=0;i<nums.length;i++)
+            {
+                if(nums[i]==0)
+                {
+                    nums[i]=pro;
+                }
+                else
+                nums[i]=0;
+            }
+        }
+
+        else{
+            for(int i=0;i<nums.length;i++)
             {
                 nums[i]=0;
             }
         }
-       }
-      else
-       {
-        for(int i=0;i<nums.length;i++)
-        {
-            nums[i]=0;
-        }
-       }
-       
-       return nums;
 
+        return nums;
     }
 }
