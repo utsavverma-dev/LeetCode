76c08a4e9aa130c606681a9e6a52afdb0d510ad7
Utsav Verma <158056139+utsavverma-dev@users.noreply.github.com>
2026-08-03 19:17:48 +0530

    Time: 4 ms (9.66%), Space: 48.1 MB (6.97%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1) return;
+        int i=0,j=i+1;
+        int n=nums.length;
+        while(i<n && j<n)
+        {
+            if(nums[i]==0 && nums[j]==0)
+            {
+                j++;
+            }
+            else if(nums[i]==0 && nums[j]!=0)
+            {
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+                i++;
+                j++;
+            }
+            else if(nums[i]!=0 && nums[j]!=0)
+            {
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+}
