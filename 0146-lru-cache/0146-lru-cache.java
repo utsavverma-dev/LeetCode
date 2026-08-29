f780a90675732654c013732b65519986c99c47ec
Utsav Verma <158056139+utsavverma-dev@users.noreply.github.com>
2026-08-29 14:26:08 +0530

    Time: 16 ms (86.24%), Space: 58.8 MB (89.85%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,82 +1,28 @@
 class LRUCache {
-
-    class Node {
-        int key, val;
-        Node prev;
-        Node next;
-
-        Node(int k, int v) {
-            key = k;
-            val = v;
-        }
-    }
-
-    Node head = new Node(-1, -1);
-    Node tail = new Node(-1, -1);
-
-    Map<Integer, Node> mp = new HashMap<>();
-    int limit;
-
+   Map<Integer,Integer>mp;
     public LRUCache(int capacity) {
-        limit = capacity;
-
-        head.next = tail;
-        tail.prev = head;
+        mp=new LinkedHashMap<>(capacity,0.3f,true)
+         {
+            @Override
+            protected boolean removeEldestEntry(
+                    Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
     }
-
-    void addNode(Node newNode) {
-        Node oldNode = head.next;
-
-        head.next = newNode;
-        newNode.prev = head;
-
-        newNode.next = oldNode;
-        oldNode.prev = newNode;
-    }
-
-    void delNode(Node oldNode) {
-        Node oldprev = oldNode.prev;
-        Node oldnext = oldNode.next;
-
-        oldprev.next = oldnext;
-        oldnext.prev = oldprev;
-    }
-
+    
     public int get(int key) {
-
-        if (!mp.containsKey(key))
-            return -1;
-
-        Node ansNode = mp.get(key);
-
-        delNode(ansNode);
-        addNode(ansNode);
-
-        return ansNode.val;
+        return mp.getOrDefault(key, -1);
     }
-
+    
     public void put(int key, int value) {
-
-        // Key already exists
-        if (mp.containsKey(key)) {
-            Node oldNode = mp.get(key);
-
-            delNode(oldNode);
-            mp.remove(key);
-        }
-
-        // Capacity reached
-        if (mp.size() == limit) {
-            Node lruNode = tail.prev;
-
-            delNode(lruNode);
-            mp.remove(lruNode.key);
-        }
-
-        // Add new node as most recently used
-        Node newNode = new Node(key, value);
-
-        addNode(newNode);
-        mp.put(key, newNode);
+        mp.put(key,value);
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
