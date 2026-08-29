0ea6c6bf1953edc0d6ccaa535dc0b05dcc2f1370
Utsav Verma <158056139+utsavverma-dev@users.noreply.github.com>
2026-08-29 17:03:42 +0530

    Time: 16 ms (86.24%), Space: 57.6 MB (98.93%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,22 +1,69 @@
 class LRUCache {
-   Map<Integer,Integer>mp;
+    int capacity;
+    Node head=new Node(0,0);
+    Node tail=new Node(0,0);
+    Map<Integer,Node> mp=new HashMap<>();
+
+    class Node {
+        Node prev;
+        Node next;
+        int key,value;
+        Node (int key,int value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+
     public LRUCache(int capacity) {
-        mp=new LinkedHashMap<>(capacity,0.3f,true)
-         {
-            @Override
-            protected boolean removeEldestEntry(
-                    Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.capacity=capacity;
+        head.next=tail;
+        tail.prev=head;
     }
     
     public int get(int key) {
-        return mp.getOrDefault(key, -1);
+
+        if(mp.containsKey(key))
+        {
+            Node node=mp.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        else
+        {
+            return -1;
+        }
+        
     }
     
     public void put(int key, int value) {
-        mp.put(key,value);
+        if(mp.containsKey(key))
+        remove(mp.get(key));
+
+        if(mp.size()==capacity)
+        remove(tail.prev);
+
+        insert(new Node(key,value));
+    }
+
+    private void remove(Node node)
+    {
+         mp.remove(node.key);
+         node.prev.next=node.next;
+         node.next.prev=node.prev;
+    }
+
+    private void insert(Node node)
+    {
+       mp.put(node.key,node);
+
+       Node headNext=head.next;
+
+       head.next=node;
+       node.prev=head;
+
+       node.next=headNext;
+       headNext.prev=node;
     }
 }
 
